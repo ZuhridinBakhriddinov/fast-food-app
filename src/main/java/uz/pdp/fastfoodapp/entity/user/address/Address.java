4b5f7877100701cb319dd25605d4dcf85341fc9f
Asil7 <asil.biohazard@gmail.com>
2022-06-09 11:04:20 +0500

    Ok

--- src/main/java/uz/pdp/fastfoodapp/entity/user/address/Address.java
+++ src/main/java/uz/pdp/fastfoodapp/entity/user/address/Address.java
@@ -14,7 +14,7 @@ import javax.persistence.ManyToOne;
 @NoArgsConstructor
 @Builder
 @Data
-@Entity(name = "adress")
+@Entity(name = "addresses")
 public class Address extends AbsEntity {
     @ManyToOne
     private User user;
