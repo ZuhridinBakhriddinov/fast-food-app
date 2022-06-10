f49c4328c317266470880036090b211f8074b827
Asil7 <asil.biohazard@gmail.com>
2022-06-10 11:28:15 +0500

    Xato joylari to'gilandi

--- src/main/java/uz/pdp/fastfoodapp/entity/user/address/AddressController.java
+++ src/main/java/uz/pdp/fastfoodapp/entity/user/address/AddressController.java
@@ -1,5 +1,46 @@
 package uz.pdp.fastfoodapp.entity.user.address;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.pdp.fastfoodapp.template.ApiResponse;
 
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/address")
 public class AddressController {
+    @Autowired
+    AddressService addressService;
+
+    @GetMapping
+    public HttpEntity<?> getAllAddress() {
+        ApiResponse allAddress = addressService.getAllAddress();
+        return ResponseEntity.ok(allAddress);
+    }
+
+    @GetMapping("/{uuid}")
+    public HttpEntity<?> getAddressById(@PathVariable UUID uuid) {
+        ApiResponse addressById = addressService.getAddressById(uuid);
+        return ResponseEntity.ok(addressById);
+    }
+
+    @PostMapping
+    public HttpEntity<?> addAddress(@RequestBody AddressDto addressDto) {
+        ApiResponse apiResponse = addressService.addAddress(addressDto);
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @PutMapping("/{addressId}")
+    public HttpEntity<?> editAddress(UUID addressId, AddressDto addressDto) {
+        ApiResponse apiResponse = addressService.editAddress(addressId, addressDto);
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @DeleteMapping("/{uuid}")
+    public HttpEntity<?> delete(UUID uuid) {
+        ApiResponse apiResponse = addressService.deleteAddreesById(uuid);
+        return ResponseEntity.ok(apiResponse);
+    }
 }
